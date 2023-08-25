1ee1706a97cde1d542520c785f59dd0a15e79814
Daniel Ferreira <pocketrocket@outlook.com.br>
2023-08-25 16:55:31 -0300

    nome das pastas corrigido

--- Aula 06/1 - area_quadrado.py
+++
@@ -1,5 +0,0 @@
-lado = float(input('Digite o valor do lado: '))
-area = lado ** 2
-print(f'Dado o lado {lado}, a área é: {area}²')
-print('Dado o lado {}, a área é: {}²'.format(lado, area))
-print('Dado o lado', lado, ', a área é: ', area)
---
+++ Aula 6/5 - calculos.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
+n3 = float(input('Digite um número real: '))
+
+r1 = (n1 * 2) * (n2 / 2)
+r2 = (3 * n1) + n3
+r3 = n3 ** 3
+
+print(f'''a: {r1}
+b: {r2}
+c: {r3}''')
